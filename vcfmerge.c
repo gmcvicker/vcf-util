dc5842ab0b02e9192c061564a6f063aa2591bbc8
Graham McVicker <gmcvicker@gmail.com>
2015-11-06 22:27:18 -0500

    fix compilation errors after FileInfo restructuring

--- vcfmerge.c
+++ vcfmerge.c
@@ -34,7 +34,7 @@ void usage(char **argv) {
 /**
  * Find subset of chromosomes that are present in all VCFs
  */
-Chromosome *chrom_table_intersect(VCFInfo **vcf, int n_vcf, int *n_intersect) {
+Chromosome *chrom_table_intersect(FileInfo *f_info, int n_vcf, int *n_intersect) {
   int i, j, k, n_chrom;
   int *counts;
   Chromosome *intersect;
@@ -43,18 +43,19 @@ Chromosome *chrom_table_intersect(VCFInfo **vcf, int n_vcf, int *n_intersect) {
     my_err("expected at least 1 vcf");
   }
 
-  counts = my_malloc(sizeof(int) * vcf[0]->n_chrom);
-  for(i = 0; i < vcf[0]->n_chrom; i++) {
+  counts = my_malloc(sizeof(int) * f_info[0].vcf->n_chrom);
+  for(i = 0; i < f_info[0].vcf->n_chrom; i++) {
     counts[i] = 1;
   }
 
   *n_intersect = 0;
 
   /* count how many other VCFs each chrom occurs in */
-  for(i = 0; i < vcf[0]->n_chrom; i++) {
+  for(i = 0; i < f_info[0].vcf->n_chrom; i++) {
     for(j = 1; j < n_vcf; j++) {
-      for(k = 0; k < vcf[j]->n_chrom; k++) {
-	if(strcmp(vcf[0]->chrom[i].name, vcf[j]->chrom[k].name) == 0) {
+      for(k = 0; k < f_info[j].vcf->n_chrom; k++) {
+	if(strcmp(f_info[0].vcf->chrom[i].name,
+		  f_info[j].vcf->chrom[k].name) == 0) {
 	  counts[i] += 1;
 	  if(counts[i] == n_vcf) {
 	    /* this chrom found in all VCFs */
@@ -68,17 +69,17 @@ Chromosome *chrom_table_intersect(VCFInfo **vcf, int n_vcf, int *n_intersect) {
 
   intersect = my_malloc(sizeof(Chromosome) * *n_intersect);
   j = 0;
-  for(i = 0; i < vcf[0]->n_chrom; i++) {
+  for(i = 0; i < f_info[0].vcf->n_chrom; i++) {
     if(counts[i] == n_vcf) {
       /* copy chrom found in all VCFs */
       intersect[j].id = j;
-      intersect[j].name = util_str_dup(vcf[0]->chrom[i].name);
-      intersect[j].assembly = util_str_dup(vcf[0]->chrom[i].assembly);
-      intersect[j].len = vcf[0]->chrom[i].len;
+      intersect[j].name = util_str_dup(f_info[0].vcf->chrom[i].name);
+      intersect[j].assembly = util_str_dup(f_info[0].vcf->chrom[i].assembly);
+      intersect[j].len = f_info[0].vcf->chrom[i].len;
       j += 1;
     } else {
       fprintf(stderr, "skipping chromosome %s because only found "
-	      "in %d/%d VCFs\n", vcf[0]->chrom[i].name, counts[i], n_vcf);
+	      "in %d/%d VCFs\n", f_info[0].vcf->chrom[i].name, counts[i], n_vcf);
     }
   }
 
@@ -91,17 +92,17 @@ Chromosome *chrom_table_intersect(VCFInfo **vcf, int n_vcf, int *n_intersect) {
 
 
 
-void init_file_info(int n_vcf, char **vcf_filenames) {
+FileInfo *init_file_info(int n_vcf, char **vcf_filenames) {
   FileInfo *f_info;
   int i, ret;
 
-  f_info = my_malloc(sizeof(FileInfo *) * n_vcf);
+  f_info = my_malloc(sizeof(FileInfo) * n_vcf);
 
   for(i = 0; i < n_vcf; i++) {
     f_info[i].vcf = vcf_info_new();
     fprintf(stderr, "reading VCF header from %s\n", vcf_filenames[i]);
     f_info[i].gzf = util_must_gzopen(vcf_filenames[i], "rb");
-    vcf_read_header(f_info[i].gzf[i], vcf[i]);
+    vcf_read_header(f_info[i].gzf, f_info[i].vcf);
     fprintf(stderr, "  VCF header lines: %ld\n", f_info[i].vcf->n_header_lines);
 
     f_info[i].is_done = FALSE;
@@ -109,14 +110,14 @@ void init_file_info(int n_vcf, char **vcf_filenames) {
     
     /* initialize memory for current SNPs */
     f_info[i].cur_snp.geno_probs =
-      my_malloc(sizeof(float) * file_info[i].vcf->n_geno_prob_col);
+      my_malloc(sizeof(float) * f_info[i].vcf->n_geno_prob_col);
     f_info[i].cur_snp.haplotypes =
-      my_malloc(sizeof(char) * file_info[i].vcf->n_haplo_col);
+      my_malloc(sizeof(char) * f_info[i].vcf->n_haplo_col);
     
     /* read first SNP from all files */
     ret = vcf_read_line(f_info[i].gzf, f_info[i].vcf, &f_info[i].cur_snp);
     if(ret == -1) {
-      my_err("file %s contains no SNPs\n", vcf_filename[i]);
+      my_err("file %s contains no SNPs\n", vcf_filenames[i]);
     }
   }
 
@@ -125,16 +126,18 @@ void init_file_info(int n_vcf, char **vcf_filenames) {
 
 
 void free_file_info(FileInfo *f_info, int n) {
+  int i;
+  
   for(i = 0; i < n; i++) {
     vcf_info_free(f_info[i].vcf);
     
-    gzclose(f_info.gzf[i]);
+    gzclose(f_info[i].gzf);
 
-    if(f_info[i].cur_snps.haplotypes) {
-      my_free(f_info[i].cur_snps.haplotypes);
+    if(f_info[i].cur_snp.haplotypes) {
+      my_free(f_info[i].cur_snp.haplotypes);
     }
-    if(f_info[i].cur_snps.geno_probs) {
-      my_free(f_info[i]..geno_probs);
+    if(f_info[i].cur_snp.geno_probs) {
+      my_free(f_info[i].cur_snp.geno_probs);
     }
   }
   my_free(f_info);
@@ -144,19 +147,18 @@ void free_file_info(FileInfo *f_info, int n) {
 
 void merge_vcf(int n_vcf, char **vcf_filenames) {
   FileInfo *f_info;
-  int n_done;
+  int n_done, n_chrom, i;
+  Chromosome *chrom_tab;
 
-  f_info = init_file_info(n_vcf, vcf_filenames):
+  f_info = init_file_info(n_vcf, vcf_filenames);
   
   /* find chromosomes that are present in ALL VCFs */
-  chrom_tab = chrom_table_intersect(f_info, &n_chrom);
+  chrom_tab = chrom_table_intersect(f_info, n_vcf, &n_chrom);
   n_done = 0;
 
   
   fprintf(stderr, "parsing files\n");
-  while(n_done < n_vcf) {
-  
-  while(vcf_read_line(gzf[0], f_info.vcf[0], &cur_snps[0]) != -1) {
+  while(vcf_read_line(f_info[0].gzf, f_info[0].vcf, &f_info[0].cur_snp) != -1) {
     /** TODO **/
     break;
   }
@@ -165,26 +167,11 @@ void merge_vcf(int n_vcf, char **vcf_filenames) {
 
   free_file_info(f_info, n_vcf);
   
-  for(i = 0; i < n_vcf; i++) {
-    vcf_info_free(vcf[i]);
-    gzclose(gzf[i]);
-
-    if(cur_snps[i].haplotypes) {
-      my_free(cur_snps[i].haplotypes);
-    }
-    if(cur_snps[i].geno_probs) {
-      my_free(cur_snps[i].geno_probs);
-    }
-  }
   for(i = 0; i < n_chrom; i++) {
     my_free(chrom_tab[i].name);
     my_free(chrom_tab[i].assembly);
   }
   my_free(chrom_tab);
-  my_free(vcf);
-  my_free(cur_snps);
-  my_free(is_done);
-  my_free(gzf);
   
 }
 
